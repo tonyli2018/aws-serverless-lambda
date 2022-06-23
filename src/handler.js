831b44dfda0e9af44ab85897db7c6db86881413b
tonyli2018 <44691952+tonyli2018@users.noreply.github.com>
2022-06-24 09:42:40 +1000

    Update handler

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports.generateRandomNumber = (event) => {
-  const randomNumber = parseInt(Math.random() * 1000);
+  const randomNumber = parseInt(Math.random() * 100);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
 };
